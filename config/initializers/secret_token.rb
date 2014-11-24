faaf5d344630fedae1c02e1582026dfeefd70870
Minh Pham <minh.pham@utexas.edu>
2014-11-24 00:54:03 -0600

    Generated boilerplate for new ruby application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toaster::Application.config.secret_key_base = '5f9dd069d7a7f7a4429c1e4c9433b1ee514454edd36abb3c5cfa9853d86ff20cda92ce9be5ff0969ded34629556ba828896714e2b8d8a908a9bd1d788b0f965a'
